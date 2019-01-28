7e0ca53b3129b5d9928390abcdd86fbde92ce78c
Vangog <ivan.rybintsev@gmail.com>
2019-01-28 09:19:21 +0600

    First commit

---
+++ StatusBarPasswordGenerator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StatusBarPasswordGenerator
+//
+//  Created by Вангог on 1/28/19.
+//  Copyright © 2019 Ivan Rybintsev. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
