553ba51e794b71d5bdfa18ae631184ae676b5754
Vangog <ivan.rybintsev@gmail.com>
2019-01-28 11:47:39 +0600

    v. 0.1

--- StatusBarPasswordGenerator/AppDelegate.swift
+++ StatusBarPasswordGenerator/AppDelegate.swift
@@ -11,12 +11,46 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    let statusItem = NSStatusBar.system.statusItem(withLength: 35.0)
+    let popover:NSPopover = NSPopover()
+    var eventMonitor:EventMonitor?
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        if let button = statusItem.button{
+            button.title = "🔐"
+            button.action = #selector(self.togglePopover(sender:))
+        }
+        popover.contentViewController = PopoverViewController.loadFromNIB()
+        eventMonitor = EventMonitor(mask: [.leftMouseUp,.rightMouseUp], handler: { (event) in
+            if self.popover.isShown{
+                self.closePopover(sender: event!)
+            }
+        })
     }
 
+    @objc func togglePopover(sender:AnyObject){
+        if popover.isShown{
+            closePopover(sender: sender)
+        }else{
+            showPopover(sender: sender)
+        }
+    }
+    
+    func closePopover(sender:AnyObject){
+        popover.performClose(sender)
+        eventMonitor?.stopMonitor()
+    }
+    
+    func showPopover(sender:AnyObject){
+        
+        if let button = statusItem.button{
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+        }
+        
+        eventMonitor?.startMonitor()
+    }
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
---
+++ StatusBarPasswordGenerator/PopoverViewController.swift
@@ -0,0 +1,82 @@
+//
+//  PopoverViewController.swift
+//  StatusBarPasswordGenerator
+//
+//  Created by Вангог on 1/28/19.
+//  Copyright © 2019 Ivan Rybintsev. All rights reserved.
+//
+
+import Cocoa
+
+class PopoverViewController: NSViewController {
+
+    class func loadFromNIB() -> NSViewController{
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateController(withIdentifier: "PopoverViewController") as! NSViewController
+        return vc
+    }
+    
+    @IBOutlet weak var upperCase: NSButton!
+    @IBOutlet weak var lowerCase: NSButton!
+    @IBOutlet weak var digits: NSButton!
+    @IBOutlet weak var autoSave: NSButton!
+    @IBOutlet weak var lengthField: NSTextField!
+    @IBOutlet weak var resultField: NSTextField!
+    @IBOutlet weak var statusLabel: NSTextField!
+    @IBOutlet weak var specials: NSButton!
+    @IBOutlet weak var exitButton: NSButton!
+    
+    @IBAction func generate(_ sender: Any) {
+        var str = ""
+        if upperCase.state == .on{
+            str.append("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        }
+        if lowerCase.state == .on{
+            str.append("abcdefghijklmnopqrstuvwxyz")
+        }
+        if digits.state == .on{
+            str.append("0123456789")
+        }
+        if specials.state == .on{
+            str.append("$%)-_@!")
+        }
+        if str.count>0{
+            let length = lengthField.integerValue
+            if length>=4{
+                var result = ""
+                for _ in 0..<length{
+                    result.append(str.randomElement()!)
+                }
+                resultField.stringValue = result
+                resultField.selectText(resultField)
+                statusLabel.textColor = NSColor.green
+                if autoSave.state == .on{
+                    let buffer = NSPasteboard.general
+                    buffer.clearContents()
+                    buffer.setString(result, forType: .string)
+                    statusLabel.stringValue = "Copied to buffer."
+                }else{
+                    statusLabel.stringValue = ""
+                }
+            }else{
+                statusLabel.textColor = NSColor.red
+                statusLabel.stringValue = "Password length invalid."
+            }
+        }else{
+            statusLabel.textColor = NSColor.red
+            statusLabel.stringValue = "Symbols empty."
+        }
+        
+    }
+    
+    @objc func closeApp(sender:AnyObject){
+        NSApplication.shared.terminate(sender)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        exitButton.action = #selector(self.closeApp(sender:))
+        // Do view setup here.
+    }
+    
+}
